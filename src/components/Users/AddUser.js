56ba4e34e64d4fa43239904b8016b82aabe8e5cf
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-08-24 17:20:08 +0200

    resetting the form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,10 +9,14 @@ function Adduser(props) {
 
   function addUserHandler(event) {
     event.preventDefault();
-    console.log(enteredUser);
-    console.log(enteredAge);
-    event.target.value = "";
-    event.target.value = "";
+    if (enteredUser.trim().length === 0 || enteredAge.trim.length === 0) {
+      return;
+    }
+    if (enteredAge < 1) {
+      return;
+    }
+    setEnteredUser("");
+    setEnteredAge("");
   }
   const userNameChangeHandler = (event) => {
     setEnteredUser(event.target.value);
@@ -28,10 +32,16 @@ function Adduser(props) {
         <input
           id="username"
           type="text"
+          value={enteredUser}
           onChange={userNameChangeHandler}
         ></input>
         <label htmlFor="username">Age(years)</label>
-        <input id="username" type="number" onChange={ageChangeHandler}></input>
+        <input
+          id="username"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        ></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
